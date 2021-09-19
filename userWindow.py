05f0b5aa747f25dccd349ab8c7ae1e161b2a0696
MangoGreenTea <hulei15082452670@gmail.com>
2021-09-19 15:30:10 +0800

    123

--- userWindow.py
+++ userWindow.py
@@ -10,7 +10,7 @@
 '''
 
 # import lib
-from PySide2.QtWidgets import QApplication, QTableWidgetItem, QMessageBox, QPushButton, QWidget, QHBoxLayout 
+from PySide2.QtWidgets import QApplication, QTableWidgetItem, QMessageBox, QPushButton, QWidget, QHBoxLayout
 from PySide2.QtUiTools import QUiLoader
 from PySide2 import QtCore
 from PySide2.QtWidgets import QApplication, QTableWidgetItem, QMessageBox
@@ -21,12 +21,13 @@ import pymysql
 con = MySQLdb()
 cur = con.cursor(cursor=pymysql.cursors.DictCursor)
 
+
 class User:
 
     def __init__(self):
         # 从文件中加载UI
         self.ui = QUiLoader().load('./ui/userWindow.ui')
-        
+
         #
         self.addShop()
         self.ui.comboBox_shop.currentIndexChanged.connect(self.shopChanged)
@@ -34,8 +35,8 @@ class User:
 
         self.setTable_commodity()
 
-
     # 分店选择
+
     def addShop(self):
         SQL_getShops = 'SELECT * FROM shop;'
         cur.execute(SQL_getShops)
@@ -45,21 +46,21 @@ class User:
 
     # 更改分店
     def shopChanged(self):
-        shopName =  self.ui.comboBox_shop.currentText()
+        shopName = self.ui.comboBox_shop.currentText()
         SQL_getShopID = 'SELECT ID FROM shop WHERE address = %s;'
-        cur.execute(SQL_getShopID,[shopName])
+        cur.execute(SQL_getShopID, [shopName])
         result_getShopID = cur.fetchone()
         self.shopID = result_getShopID['ID']
-        
+
     # 设置商品表格
     def setTable_commodity(self):
         # 查询商品信息和库存
         SQL_getCommodity = 'SELECT commodity.ID,commodity.Names,commodity.Brand,commodity.Weights,commodity.Types,stock.Amount,commodity.Price FROM commodity,stock WHERE commodity.ID = stock.commodityID AND stock.shopID = %s;'
         cur1 = con.cursor()
-        cur1.execute(SQL_getCommodity,[self.shopID])
+        cur1.execute(SQL_getCommodity, [self.shopID])
         result_getCommodity = cur1.fetchall()
-        row = cur1.rowcount  #取得记录个数，用于设置表格的行数
-        vol = len( result_getCommodity[0] )  #取得字段数，用于设置表格的列数
+        row = cur1.rowcount  # 取得记录个数，用于设置表格的行数
+        vol = len(result_getCommodity[0])  # 取得字段数，用于设置表格的列数
 
         # 设置表格长宽
         self.ui.tableWidget_commodity.setRowCount(row)
@@ -76,33 +77,34 @@ class User:
         self.ui.tableWidget_commodity.setColumnWidth(7, 150)
         self.ui.tableWidget_commodity.setColumnWidth(8, 150)
 
-
         # 设置表头
         self.ui.tableWidget_commodity.setHorizontalHeaderLabels(
             ['图片', '条形码', '商品名称', '商品品牌', '净含量(g/ml)', '商品种类', '商品库存', '单价(元)', '操作'])
 
-        self.ui.tableWidget_commodity.setHorizontalHeaderLabels(['图片', '条形码', '商品名称', '商品品牌', '净含量(g/ml)', '商品种类', '商品库存', '单价(元)', '操作'])
+        self.ui.tableWidget_commodity.setHorizontalHeaderLabels(
+            ['图片', '条形码', '商品名称', '商品品牌', '净含量(g/ml)', '商品种类', '商品库存', '单价(元)', '操作'])
 
         self.ui.tableWidget_commodity.horizontalHeader().setStyleSheet(
             "QHeaderView::section{background-color:rgb(155, 194, 230);font:11pt '宋体';color: black;};")
 
         # 插入数据
         for i in range(row):
-            self.ui.tableWidget_commodity.setCellWidget(i, 8,self.buttonForRow(str(result_getCommodity[0])))
+            self.ui.tableWidget_commodity.setCellWidget(
+                i, 8, self.buttonForRow(str(result_getCommodity[0])))
             for j in range(vol):
                 # 临时记录，不能直接插入表格，转换后可插入表格
-                temp_data=result_getCommodity[i][j]
+                temp_data = result_getCommodity[i][j]
                 # 转换
-                data = QTableWidgetItem(str(temp_data)) 
+                data = QTableWidgetItem(str(temp_data))
                 # 插入
-                self.ui.tableWidget_commodity.setItem(i,j+1,data)
+                self.ui.tableWidget_commodity.setItem(i, j+1, data)
                 # 居中
-                self.ui.tableWidget_commodity.item(i,j+1).setTextAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter);
-
+                self.ui.tableWidget_commodity.item(
+                    i, j+1).setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
 
     # 列表内添加按钮
-    def buttonForRow(self,id):
-        widget=QWidget()
+    def buttonForRow(self, id):
+        widget = QWidget()
         # 修改
         button_update = QPushButton('+')
         button_update.setStyleSheet(''' text-align : center;
@@ -111,7 +113,7 @@ class User:
                                           border-style: outset;
                                           font : 13px  ''')
 
-        button_update.clicked.connect(lambda:self.updateTable(id))
+        button_update.clicked.connect(lambda: self.updateTable(id))
 
         # 查看
         button_view = QPushButton('-')
@@ -131,32 +133,14 @@ class User:
                                     border-style: outset;
                                     font : 13px; ''')
 
-
         hLayout = QHBoxLayout()
         hLayout.addWidget(button_update)
 
         hLayout.addWidget(button_view)
         hLayout.addWidget(button_delete)
-        hLayout.setContentsMargins(5,2,5,2)
+        hLayout.setContentsMargins(5, 2, 5, 2)
         widget.setLayout(hLayout)
         return widget
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app = QApplication([])
